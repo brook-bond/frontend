b8fcab5596dc1c26cafd9ecd98256442f73cf031
brook-bond <haykazvoskanyan@gmail.com>
2023-08-25 16:08:56 +0400

    crud

--- src/components/ProtectedLayout.jsx
+++ src/components/ProtectedLayout.jsx
@@ -95,7 +95,28 @@ export default function DefaultLayout() {
 									About
 								</NavLink>
 							</li>
-
+							<li>
+								<NavLink
+									to="/productlist"
+									className={({ isActive }) =>
+										isActive
+											? 'block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white'
+											: 'block py-2 pl-3 pr-4 rounded md:bg-transparent md:p-0 dark:text-gray-400 md:dark:hover:text-white'
+									}>
+									Product List
+								</NavLink>
+							</li>
+							<li>
+								<NavLink
+									to="/addproduct"
+									className={({ isActive }) =>
+										isActive
+											? 'block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white'
+											: 'block py-2 pl-3 pr-4 rounded md:bg-transparent md:p-0 dark:text-gray-400 md:dark:hover:text-white'
+									}>
+									Add Product
+								</NavLink>
+							</li>
 							<li>
 								<a
 									onClick={handleLogout}
--- src/router.jsx
+++ src/router.jsx
@@ -5,6 +5,9 @@ import Profile from './pages/Profile';
 import Register from './pages/Register';
 import ProtectedLayout from './components/ProtectedLayout';
 import GuestLayout from './components/GuestLayout';
+import Addproduct from './pages/Addproduct';
+import Productlist from './pages/Productlist';
+import EditProduct from './pages/EditProduct';
 
 const router = createBrowserRouter([
 	{
@@ -33,6 +36,18 @@ const router = createBrowserRouter([
 				path: '/profile',
 				element: <Profile />,
 			},
+			{
+				path: '/addproduct',
+				element: <Addproduct />,
+			},
+			{
+				path: '/productlist',
+				element: <Productlist />,
+			},
+			{
+				path: '/editproduct/:id/edit',
+				element: <EditProduct />,
+			}
 		],
 	},
 ]);
